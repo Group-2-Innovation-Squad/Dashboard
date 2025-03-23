63d3526aa2304c40359e1c82fc533a95e017c4c0
Nathaniel Luffey <nathanielluffey161@gmail.com>
2025-03-23 02:42:37 -0400

    added initial design shell

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="topBar">
+        <span className="title">GREENHOUSE ASSISTANT DASHBOARD</span>
+        <div className="iconContainer">
+          <ion-icon className="icon" name="home-sharp"></ion-icon>
+          <ion-icon className="icon" name="image-sharp"></ion-icon>
+          <ion-icon className="icon" name="notifications-sharp"></ion-icon>
+        </div>
+      </div>
+      <div className="subBarContainer">
+        <div className="statContainer">
+          <span className="subTitle">Last Temp Value: </span>
+        </div>
+        <div className="statContainer">
+          <span className="subTitle">Last Humidity Value: </span>
+        </div>
+      </div>
+      <div className="subSubBarContainer">
+        <div className="messagesContainerTitle">
+          <span className="subTitle">Important Messages</span>
+        </div>
+      </div>
+      <div className="subSubSubBarContainer">
+        <div className="messagesContainer">
+          <span className="subTitle">Sample Messages</span>
+        </div>
+      </div>
+      <div className="bottomBar">
+        <span className="subTitle">Feedback</span>
+      </div>
     </div>
   );
 }
